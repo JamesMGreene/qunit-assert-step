c3ebfdcb2b130623cb610d5fc2915631263346d9
James M. Greene <james.m.greene@gmail.com>
2016-10-02 22:13:40 -0500

    Updated devDeps

--- test/step-test.js
+++ test/step-test.js
@@ -1,19 +1,26 @@
-QUnit.module("qunit-assert-step plugin unit tests");
+(function( module, test ) {
 
-QUnit.test("step", function(assert) {
-  QUnit.expect(3);
+  module("qunit-assert-step plugin unit tests");
 
-  assert.step(1, "step starts at 1");
-  setTimeout(function() {
-    QUnit.start();
-    assert.step(3);
-  }, 100);
-  assert.step(2, "before the setTimeout callback is run");
-  QUnit.stop();
-});
+  test("step", function(assert) {
+    var done;
 
-QUnit.test("step counter resets", function(assert) {
-  QUnit.expect(1);
+    assert.expect(3);
 
-  assert.step(1, "each test has its own step counter");
-});
+    assert.step(1, "step starts at 1");
+    setTimeout(function() {
+      assert.step(3, "inside of the setTimeout callback");
+      done();
+    }, 13);
+    assert.step(2, "before the setTimeout callback is run");
+
+    done = assert.async();
+  });
+
+  test("step counter resets", function(assert) {
+    assert.expect(1);
+
+    assert.step(1, "each test has its own step counter");
+  });
+
+})( QUnit.module, QUnit.test );
